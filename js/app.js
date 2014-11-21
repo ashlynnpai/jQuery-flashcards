4ba2ad2bc2ed178b85c0c162520d2914e0db4452
Ashlynn Pai <ashlynnpai@gmail.com>
2014-11-20 21:40:52 -0500

    second commit

--- js/app.js
+++ js/app.js
@@ -3,33 +3,28 @@ var set = [];
 var origset = [];
 var Card; 
 
-  $.getJSON('js/set.json', function(data) {
-      $.each(data, function(index, item) {
-          console.log(data);
-          set.push(item);
-          console.log(set);
-          cycle();
-      });
-  });
+$.getJSON('js/set.json', function(data) {
+    $.each(data, function(index, item) {
+        set.push(item);
+        cycle();
+    });
+});
 
- var origset = set.slice(0);
+var origset = set.slice(0);
        
 function cycle(){
-  console.log(set);
-  Card = set.shift();  
-  $("#card").html(Card.front);
-  set.push(Card);
+    Card = set.shift();  
+    $("#card").html(Card.front);
+    set.push(Card);
 }
 
-
- 
 $("#card").click(function(){
     $("#card").html($("#card").text() == Card.front ? Card.back : Card.front);
     });
 
 $( "#nextbutton" ).click(function() {
-  cycle();
-   });        
+    cycle();
+     });        
 
 $( "#knownbutton" ).click(function() {
   set.pop();
